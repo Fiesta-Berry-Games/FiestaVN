25ef3e05ec6670ba36586579e5c2b43b78971038
sneurlax <sneurlax@gmail.com>
2026-03-03 20:42:51 -0600

    style: format renfly image sprite

--- apps/renfly/lib/widgets/image_sprite.dart
+++ apps/renfly/lib/widgets/image_sprite.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ImageSprite extends StatelessWidget {
-  const ImageSprite({
-    super.key,
-    required this.imagePath,
-    required this.atLeft,
-  });
+  const ImageSprite({super.key, required this.imagePath, required this.atLeft});
 
   final String imagePath;
   final bool atLeft;
@@ -41,18 +37,12 @@ class ImageSprite extends StatelessWidget {
                 const SizedBox(height: 8),
                 Text(
                   'Image not found',
-                  style: TextStyle(
-                    color: Colors.grey.shade300,
-                    fontSize: 12,
-                  ),
+                  style: TextStyle(color: Colors.grey.shade300, fontSize: 12),
                 ),
                 const SizedBox(height: 4),
                 Text(
                   imagePath.split('/').last,
-                  style: TextStyle(
-                    color: Colors.grey.shade400,
-                    fontSize: 10,
-                  ),
+                  style: TextStyle(color: Colors.grey.shade400, fontSize: 10),
                   textAlign: TextAlign.center,
                 ),
               ],
